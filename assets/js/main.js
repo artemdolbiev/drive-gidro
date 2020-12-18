af67cc7241831b6af8f00d0a0ffe2b2876f79960
Artem Dolbiev <dolbievartem@gmail.com>
2020-12-18 11:48:58 +0200

    Закончена секция поиска и категорий

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,21 @@ $(function(){
 
   $('.banner-section__slider').slick({
     dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    speed: 500,
     prevArrow: ' <button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="assets/images/arrow-left.svg" alt=""></button>',
     nextArrow: ' <button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="assets/images/arrow-right.svg" alt=""></button>',
+  });
+
+  $('.tab').on('click', function(e){
+    e.preventDefault();
+
+    $('.tab').removeClass('tab--active');
+    $('.tabs-content').removeClass('tabs-content--active');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
   });
 });
